2f4e5cc543b056d6c77b2de879f1489ab2663135
hmicn <michael@xonatis.com>
2024-12-08 14:10:59 +0100

    Fixing storage token

---
+++ assets/educentre-storage.js
@@ -0,0 +1,45 @@
+(function (global) {
+    function EducentreStorage() {
+        this.listeners = {};
+
+        this.generateId = function () {
+            return 'xxxxxx'.replace(/[x]/g, function () {
+                return ((Math.random() * 16) | 0).toString(16);
+            });
+        };
+
+        window.addEventListener("message", (event) => {
+            if (!event || !event.data) return;
+            const message = event.data;
+            if (message.requestId && this.listeners[message.requestId]) {
+                this.listeners[message.requestId](message.payload);
+                delete this.listeners[message.requestId];
+            }
+        });
+    }
+
+    /**
+     * Envoie un message au parent et attend une réponse.
+     * @param {string} action - Action ou type de message.
+     * @param {object} data - Données à envoyer.
+     * @param {function} callback - Fonction appelée avec la réponse.
+     */
+    EducentreStorage.prototype.request = function (action, data, callback) {
+        const requestId = this.generateId();
+        const message = { requestId, action, data };
+        this.listeners[requestId] = callback;
+        window.parent.postMessage(message, '*');
+    };
+
+    /**
+     * Envoie un message au parent sans attendre de réponse.
+     * @param {string} action - Action ou type de message.
+     * @param {object} data - Données à envoyer.
+     */
+    EducentreStorage.prototype.send = function (action, data) {
+        const message = { action, data };
+        window.parent.postMessage(message, '*');
+    };
+
+    global.EducentreStorage = EducentreStorage;
+})(window);
